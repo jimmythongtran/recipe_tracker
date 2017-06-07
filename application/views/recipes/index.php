bfaeba855ed30e6c27e3a14e8396884daa199f25
Jimmy Tran <jimmytranman@gmail.com>
2017-06-07 16:33:01 -0700

    Added author's name

--- application/views/recipes/index.php
+++ application/views/recipes/index.php
@@ -4,7 +4,8 @@
         <div class="list-group-item clearfix">
             <h4 class="list-group-item-heading"><?php echo $recipe->name; ?></h4>
         <p class="list-group-item-text">
-            <?php echo $recipe->time_cook; ?>
+            <?php echo $recipe->time_cook; ?><br>
+            <?php echo $recipe->author_name; ?><br>
         </p>
         </div>
     <?php } ?>
