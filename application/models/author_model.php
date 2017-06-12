110130d83d4e194c1401a31cffa2b41a0b5e3c16
Jimmy Tran <jimmytranman@gmail.com>
2017-06-11 17:12:27 -0700

    Added author and step models

---
+++ application/models/author_model.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Author_model extends CI_Model {
+
+    /**
+     * Author fields
+     */
+    var $name = '';
+}
---
+++ application/models/step_model.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Step_model extends CI_Model {
+
+    /**
+     * Step fields
+     */
+
+    var $recipe_id = '';
+    var $instructions = '';
+    var $order = '';
+}
