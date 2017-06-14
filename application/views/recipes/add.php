5ab62642591ca260bcb4aca3b00aa55252d1e27c
Jimmy Tran <jimmytranman@gmail.com>
2017-06-14 16:42:45 -0700

    Created form_inputs, dropdowns, astericks

---
+++ application/views/recipes/add.php
@@ -0,0 +1,49 @@
+<h1>Add a recipe</h1>
+
+<form role="form">
+
+    <div class="form-group">
+        <label>Name<span class="text-danger">*</span></label>
+        <?php
+        echo form_input(array(
+            'name' => 'name',
+            'class' => 'form-control'
+        ));
+        ?>
+    </div>
+
+    <div class="form-group">
+        <label>Author<span class="text-danger">*</span></label>
+        <?php
+        echo form_dropdown('author_id', array(), '', 'class="form-control"');
+        ?>
+    </div>
+
+    <div class="form-group form-time clearfix">
+        <div><label>Prep Time<span class="text-danger">*</span></label></div>
+        <?php
+        echo form_dropdown('time_prep_hours', array(), '', 'class="form-control"');
+        ?>
+        <label class="select-label">hr.</label>
+        <?php
+        echo form_dropdown('time_prep_minutes', array(), '',
+            'class="form-control"');
+        ?>
+        <label class="select-label">min.</label>
+    </div>
+
+    <div class="form-group form-time clearfix">
+        <div><label>Cook Time<span class="text-danger">*</span></label></div>
+        <?php
+        echo form_dropdown('time_cook_hours', array(), '', 'class="form-control"');
+        ?>
+        <label class="select-label">hr.</label>
+        <?php
+        echo form_dropdown('time_cook_minutes', array(), '', 'class="form-control"');
+        ?>
+        <label class="select-label">min.</label>
+    </div>
+
+    <button type="submit" class="btn btn-lg btn-primary">Add Recipe</button>
+
+</form>
