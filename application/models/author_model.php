b52729c2c28fcca928dc5ca6d7eebfcad7b44afe
Jimmy Tran <jimmytranman@gmail.com>
2017-06-14 17:16:17 -0700

    Added function to get all authors from database

--- application/models/author_model.php
+++ application/models/author_model.php
@@ -6,4 +6,15 @@ class Author_model extends CI_Model {
      * Author fields
      */
     var $name = '';
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    // retrieve all authors
+    function get_all_entries() {
+        $query = $this->db->get('authors');
+        $result = $query->result();
+        return $result;
+    }
 }
