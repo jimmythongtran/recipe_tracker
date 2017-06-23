888eb503622cf834c68b95f90a02383b4c6569f3
Jimmy Tran <jimmytranman@gmail.com>
2017-06-23 13:36:50 -0700

    Added ingredient add button, not working

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,15 @@
 
 /* Custom JavaScript will be placed in this file */
+$(document).ready(function() {
+
+    /**
+     * Add recipe ingredients
+     */
+
+    var $addIngredientForm = $('#add_ingredient_form');
+
+    $('#show_add_ingredient').click(function(e) {
+        e.preventDefault();
+        $addIngredientForm.toggle();
+    });
+});
