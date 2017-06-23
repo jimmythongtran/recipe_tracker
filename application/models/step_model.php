791b26b441e4c3170559a0bc12f21d8a3137296a
Jimmy Tran <jimmytranman@gmail.com>
2017-06-23 11:27:58 -0700

    Added get_recipe_entries()

--- application/models/step_model.php
+++ application/models/step_model.php
@@ -9,4 +9,11 @@ class Step_model extends CI_Model {
     var $recipe_id = '';
     var $instructions = '';
     var $order = '';
-}
+
+    function get_recipe_entries($recipe_id) {
+        $query = $this->db->order_by('order', 'ASC')
+                ->get_where('steps', array('recipe_id' => $recipe_id));
+        $result = $query->result();
+        return $result;
+    } //end get_recipe_entries
+} // end Step_model
