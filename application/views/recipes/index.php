a64020dcbcfc47fabf8dc0c364b8ef408ce33378
Jimmy Tran <jimmytranman@gmail.com>
2017-06-22 20:09:03 -0700

    Can click to view single entries

--- application/views/recipes/index.php
+++ application/views/recipes/index.php
@@ -5,14 +5,14 @@
 
 <?php echo $this->session->flashdata('message'); ?>
 
-<div class="list-group">
+<div class="list-group recipes-list">
     <?php foreach ($recipes as $recipe) { ?>
-        <div class="list-group-item clearfix">
-            <h4 class="list-group-item-heading"><?php echo $recipe->name; ?></h4>
+    <a class="list-group-item clearfix" href="<?php echo site_url('recipes/' . $recipe->id); ?>">
+        <h4 class="list-group-item-heading"><?php echo $recipe->name; ?></h4>
         <p class="list-group-item-text">
             <?php echo $recipe->time_cook; ?><br>
             <?php echo $recipe->author_name; ?><br>
         </p>
-        </div>
+        </a>
     <?php } ?>
 </div>
