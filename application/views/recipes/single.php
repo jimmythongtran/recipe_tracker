6b30a5186153d59ab2eaee802994dac556f65fae
Jimmy Tran <jimmytranman@gmail.com>
2017-06-22 19:41:31 -0700

    Added single recipe pages with basic info showing

--- application/views/recipes/single.php
+++ application/views/recipes/single.php
@@ -1,1 +1,32 @@
-<h1>Single recipe #<?php echo $recipe_id; ?> page coming soon...</h1>
+<h1><?php echo $recipe->name; ?></h1>
+
+<div class="row recipe-quick-facts">
+    <div class="col-md-4">
+        <h4>Servings</h4>
+        <?php echo $recipe->servings; ?>
+    </div>
+    <div class="col-md-4">
+        <h4>Prep Time</h4>
+        <?php echo display_time($recipe->time_prep); ?>
+    </div>
+    <div class="col-md-4">
+        <h4>Cook Time</h4>
+        <?php echo display_time($recipe->time_cook); ?>
+    </div>
+</div>
+
+<div class="recipe-ingredients">
+
+    <h2>Ingredients</h2>
+
+    <p>Ingredients will go here...</p>
+
+</div><!-- /.recipe-ingredients -->
+
+<div class="recipe-steps">
+
+    <h2>Steps</h2>
+
+    <p>Steps will go here...</p>
+
+</div><!-- /.recipe-steps -->
