b14ccb884c115a2c9283c07ee75b008d6156af57
Jimmy Tran <jimmytranman@gmail.com>
2017-06-23 11:26:04 -0700

    Added get_recipe_entries()

--- application/models/ingredient_model.php
+++ application/models/ingredient_model.php
@@ -11,4 +11,11 @@ class Ingredient_model extends CI_Model {
     var $amount = '';
     var $unit = '';
     var $order = '';
-}
+
+    function get_recipe_entries($recipe_id) {
+        $query = $this->db->order_by('order', 'ASC')
+            ->get_where('ingredients', array('recipe_id' => $recipe_id));
+        $result = $query->result();
+        return $result;
+    } // end get_recipe_entries
+} // end Ingredient_model
