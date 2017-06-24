65be7f2236d0fe0632bbbcfbeec0459018117c64
Jimmy Tran <jimmytranman@gmail.com>
2017-06-24 09:49:44 -0700

    Adding ingredients, submit no data shows red

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,63 @@
 
-/* Custom JavaScript will be placed in this file */
 $(document).ready(function() {
-    /**
-     * Add recipe ingredients
-     */
-    var $addIngredientForm = $('#add_ingredient_form');
 
-    $('#show_add_ingredient').click(function(e) {
-        e.preventDefault();
-        $addIngredientForm.toggle();
-    });
-});
+  /**
+   * Add recipe ingredients
+   */
+
+  var $addIngredientForm = $('#add_ingredient_form');
+
+  $('#show_add_ingredient').click(function(e) {
+    e.preventDefault();
+    $addIngredientForm.toggle();
+  });
+
+  $addIngredientForm.submit(function(e) {
+    e.preventDefault();
+
+    // Get the form's URL from its "action" attribute
+    // that was set with the proper url using form_open()
+    var formURL = $addIngredientForm.attr('action');
+
+    // Get the form inputs' values in a string
+    // formatted as name1=value1&name2=value2, etc.
+    var formData = $addIngredientForm.serialize();
+
+    // Get the current number of ingredients
+    // and set our new ingredient's order to that number + 1
+    var $ingredientList = $('#ingredients');
+    var ingredientOrder = $ingredientList.find('li').length + 1;
+
+    // Add the order value to our form data
+    formData = formData + "&order=" + ingredientOrder;
+
+    // Submit the form data via Ajax
+    $.post(
+      formURL,
+      formData,
+      function(json) {
+        // Clear any previous errors
+        var $addIngredientErrors = $('#add_ingredient_errors');
+        $addIngredientErrors.empty();
+
+        // Act according to the status returned
+        var status = json["status"];
+        if (status == "success") {
+          var ingredientHTML = json["ingredient"];
+          $ingredientList.append(ingredientHTML);
+
+          // Reset the text input values and hide the form
+          $addIngredientForm.find('input[type="text"]').val('');
+          $addIngredientForm.hide();
+        }
+        else if (status == "fail") {
+          var errorsHTML = json["errors"];
+          $addIngredientErrors.html(errorsHTML);
+        }
+      },
+      'json'
+    );
+
+  });
+
+}); // end JavaScript
