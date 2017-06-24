9d7ddc7992d48dd765b54251603b84facfcf3b47
Jimmy Tran <jimmytranman@gmail.com>
2017-06-23 20:46:06 -0700

    Fixed the +Add ingredient button

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,9 @@
 
 /* Custom JavaScript will be placed in this file */
 $(document).ready(function() {
-
     /**
      * Add recipe ingredients
      */
-
     var $addIngredientForm = $('#add_ingredient_form');
 
     $('#show_add_ingredient').click(function(e) {
