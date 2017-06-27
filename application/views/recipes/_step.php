aacca036daf57ab475cd73bffa11538a32eb29a5
Jimmy Tran <jimmytranman@gmail.com>
2017-06-26 18:02:59 -0700

    Delete step buttons showing

--- application/views/recipes/_step.php
+++ application/views/recipes/_step.php
@@ -1,4 +1,6 @@
-<li class="list-group-item">
+<li class="list-group-item" data-type="step" data-id="<?php echo $step->id; ?>">
+<button class="btn btn-danger btn-xs pull-right delete-recipe-item" type="button">
+<i class="glyphicon glyphicon-remove"></i></button>
     <?php
     echo $step->instructions;
     ?>
