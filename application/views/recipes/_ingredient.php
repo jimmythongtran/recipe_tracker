35a2d4b59a73aca4fdd88860caba29c613a9bb79
Jimmy Tran <jimmytranman@gmail.com>
2017-06-26 18:00:18 -0700

    Delete buttons showing

--- application/views/recipes/_ingredient.php
+++ application/views/recipes/_ingredient.php
@@ -1,7 +1,7 @@
-<li class="list-group-item">
+<li class="list-group-item" data-type="ingredient" data-id="<?php echo $ingredient->id; ?>">
+<button class="btn btn-danger btn-xs pull-right delete-recipe-item" type="button">
+<i class="glyphicon glyphicon-remove"></i></button>
     <?php
-    echo $ingredient->amount
-        . ' ' . $ingredient->unit
-        . ' ' . $ingredient->name;
+    echo $ingredient->amount . ' ' . $ingredient->unit . ' ' . $ingredient->name;
     ?>
 </li>
